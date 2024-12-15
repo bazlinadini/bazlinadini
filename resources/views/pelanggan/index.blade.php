69c1b1f964e9a61d4ef1388d48086d8fd4d1c08e
bazlinadini <bazlinadini043@gmail.com>
2024-12-15 22:43:05 +0700

    membuat tabel pada data pelanggan

--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -27,6 +27,33 @@
             </li>
         </ul>
         
+        <table class="table table-striped table-hover col-sm-12">
+                <h4>Tabel Data Pelanggan</h4>
+                <thead>
+                    <td>No</td>
+                    <td>Nama</td>
+                    <td>Alamat</td>
+                    <td>HandPhone</td>
+                    <td>Golongan</td>
+                    <td>Aksi</td>
+                </thead>
+                <tbody>
+                    <td>1</td>
+                    <td>Bazlina Dini Amanda</td>
+                    <td>Kesatuan, Kec. Perbaungan</td>
+                    <td>0838-6724-8591</td>
+                    <td>Pimpinan</td>
+                    <td>Delete</td>
+                </tbody>
+                <tbody>
+                    <td>2</td>
+                    <td>Raihan Maulana Irsyad</td>
+                    <td>Batang Terap, Kec. Perbaungan</td>
+                    <td>0822-7515-7653</td>
+                    <td>Pimpinan</td>
+                    <td>Delete</td>
+                </tbody>
+        </table>
     </div>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </body>
